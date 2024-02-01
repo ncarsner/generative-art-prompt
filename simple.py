3037f2f3a057bd9ae573c909ddd9c2f418d2c6d7
Nicholas Carsner <nicholascarsner@gmail.com>
2024-02-01 16:30:57 -0600

    Created a shorter, simpler prompt.

---
+++ simple.py
@@ -0,0 +1,36 @@
+import random
+import component_words as words
+
+
+def generate_prompt_simple():
+    component_lists = {
+        "Color Palettes": words.color_palettes, # black and white, multicolor, pastel
+        "Artistic Styles": words.artistic_styles, # art deco, futurist
+        "Mediums": words.mediums, # watercolor, acrylic
+        "Subject": words.subjects,
+        "Shape": words.subjects,
+    }
+
+    # entry_quantifier = "a single portrait orientation image of "
+    # exit_qualifier = " at 300 DPI with a transparent background"
+    components = []
+
+    for label, comp_list in component_lists.items():
+        if random.choice([True]):
+            choice = random.choice(comp_list)
+            component = {
+                "Color Palettes": f"{choice}",
+                "Artistic Styles": f"{choice}",
+                "Mediums": f"{choice}",
+                "Subject": f"{choice}",
+                "Shape": f"in the shape of a {choice}",
+            }.get(label, "")
+            components.append(component)
+
+    # return entry_quantifier + " ".join(components) + exit_qualifier if components else "No components selected."
+    # return entry_quantifier + " ".join(components) if components else "No components selected."
+    return " ".join(components) if components else "No components selected."
+
+if __name__ == "__main__":
+    for _ in range(10):
+        print(generate_prompt_simple())
